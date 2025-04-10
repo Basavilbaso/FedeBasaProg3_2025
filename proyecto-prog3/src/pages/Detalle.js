09c2e1eb864d8fdc31cddffdcf077f000bd2689a
Pikipoggio1 <mpoggio@udesa.edu.ar>
2025-04-10 18:01:00 -0300

    Detalle terminado pt.5

--- proyecto-prog3/src/pages/Detalle.js
+++ proyecto-prog3/src/pages/Detalle.js
@@ -1,14 +1,68 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { apikey } from "../utils";
+
 
 class Detalle extends Component {
-  
-  render() {
+    constructor(props) {
+        super(props);
+        this.state = {
+            pelicula: null
+        };
+    }
 
-    return(
-       <p>Pagina de detalle</p>
-    )
-  }
+    componentDidMount() {
+        const id = Number(this.props.match.params.id);
+        fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=` + apikey)
+            .then(res => res.json())
+            .then(data => {
+                this.setState({
+                    pelicula: data
+                });
+            })
+            .catch(error => console.log(error));
+    }
+
+
+
+    render() {
+        const { pelicula } = this.state;
+
+        return (
+            <>
+                {
+                    !pelicula &&
+                    <p>Cargando...</p>
+
+                }
+                {pelicula &&
+                    <section className="section-detalle">
+                        <article className="info">
+                            <h3 className="tituloProd">
+                                <Link to={`/Detail/id/${pelicula.id}`}>{pelicula.title}</Link>
+                            </h3>
+                            <img className="foto" src={'https://image.tmdb.org/t/p/w342' + pelicula.poster_path} alt="foto" />
+                            <p>Rating: {pelicula.vote_average}</p>
+                            <p>Fecha de lanzamiento: {pelicula.release_date}</p>
+                            <p>Duración: {pelicula.runtime} min</p>
+                            <p>Sinopsis: {pelicula.overview}</p>
+                            {pelicula.genres && pelicula.genres.length > 0 && (
+                                <div>
+                                    <p>Géneros:</p>
+                                    {pelicula.genres.map((genre, idx) => (
+                                        <p key={idx}>{genre.name}</p>
+                                    ))}
+                                </div>
+                            )}
+
+                            <button className="boton-favorito">Agregar a favoritos</button>
+                        </article>
+                    </section>
+
+                }
+            </>
+        )
+    }
 }
 
 export default Detalle;
