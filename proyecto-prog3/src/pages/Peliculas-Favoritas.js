2b8855a6d6dfc71f0a8d5006055840e0a05ae668
Pikipoggio1 <mpoggio@udesa.edu.ar>
2025-04-13 23:25:38 -0300

    Pag Favoritas terminada

--- proyecto-prog3/src/pages/Peliculas-Favoritas.js
+++ proyecto-prog3/src/pages/Peliculas-Favoritas.js
@@ -1,11 +1,74 @@
 import React from "react";
 import { Component } from "react";
+import { apikey } from "../utils";
+import Pelicula from "../components/Pelicula/Pelicula"
 
-class Favoritos extends Component{
-    render(){
-        return(
-            <p>Pagina de peliculas favoritas</p>
-        )
+
+class Favoritos extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            peliculas: [],
+            haypelis: false
+        }
     }
+
+    componentDidMount() {
+        const storage = localStorage.getItem('favoritos');
+        if (storage !== null) {
+            const parsedStorage = JSON.parse(storage);
+            if (parsedStorage.length > 0) {
+                Promise.all(
+                    parsedStorage.map((id) =>
+                        fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=` + apikey)
+                            .then(response => response.json())
+                            .catch(e => console.log(e)))
+                )
+                    .then((data) => {
+                        this.setState({
+                            peliculas: data,
+                            haypelis: true
+                        })
+                    })
+                    .catch(error => console.log(error));
+            }
+
+        }
+    }
+
+    filtrarPeliculasFavoritas(id) {
+        const peliculasFiltradas = this.state.peliculas.filter(
+            elm => elm.id !== id
+        )
+        this.setState({ peliculas: peliculasFiltradas })
+    }
+
+    render() {
+        return (
+            <>
+                <h1>Películas Favoritas</h1>
+                <section>
+                {
+                    
+                    this.state.peliculas.length > 0 ?
+                        this.state.peliculas.map((pelicula, idx) =>
+                            <Pelicula
+                                peliculas={pelicula}
+                                key={idx}
+                                borrarDeFavoritos={(id) => this.filtrarPeliculasFavoritas(id)}
+                            />)
+                        :
+                        this.state.haypelis === false ?
+                            <h1>No se encuentran peliculas seleccionandas como favoritas.</h1>
+                            :
+                            <h1>Cargando...</h1>
+                }
+                </section>
+            </>
+        );
+    }
+
+
 }
 export default Favoritos;
+//prueba 
